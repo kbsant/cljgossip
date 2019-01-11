492b143e184d33d3bfd0f15e6ebb2d1c4766b87d
kbsant <keansant@gmail.com>
2019-01-12 08:48:43 +0900

    add emoji api ❄️

--- src/cljgossip/emojiapi.clj
+++ src/cljgossip/emojiapi.clj
@@ -3,6 +3,14 @@
 
 (def 🌈 "Synonym for login" core/login)
 
-(def ⚡️ "Synonym for send" core/send-to)
+(def ⚡️ "Synonym for send-to" core/send-to)
 
-(def ⭐️  "Synonym for send-all" core/send-all)
+(def ⭐️ "Synonym for send-all" core/send-all)
+
+(def ✨ "Synonym for sign-in" core/sign-in)
+
+(def ❄️ "Synonym for sign-out" core/sign-out)
+
+(def 🔭 "Synonym for status" core/status)
+
+(def 💔 "Synonym for close" core/close)
--- test/cljgossip/emojiapi_test.clj
+++ test/cljgossip/emojiapi_test.clj
@@ -7,4 +7,8 @@
   (testing "Emojis resolve into the api functions."
     (is (= emo/🌈 core/login))
     (is (= emo/⭐️ core/send-all))
-    (is (= emo/⚡️ core/send-to))))
+    (is (= emo/⚡️ core/send-to))
+    (is (= emo/✨ core/sign-in))
+    (is (= emo/❄️ core/sign-out))
+    (is (= emo/🔭 core/status))
+    (is (= emo/💔 core/close))))
