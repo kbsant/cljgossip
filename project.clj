57ec11477f601dc8325e6c979b32aa20f83652f9
kbsant <keansant@gmail.com>
2022-09-04 12:26:20 +0900

    bump deps

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "MUD Gossip client library in clojure"
   :url "https://github.com/kbsant/cljgossip"
   :dependencies
-  [[org.clojure/clojure "1.10.0"]
-   [org.clojure/tools.logging "0.4.1"]
-   [medley "1.0.0"]]
+  [[org.clojure/clojure "1.11.1"]
+   [org.clojure/tools.logging "1.2.4"]
+   [medley "1.4.0"]]
   :repl-options {:init-ns cljgossip.core})
